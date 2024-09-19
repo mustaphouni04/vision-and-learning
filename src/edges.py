74cfce2315bb43fc56a129826b4a1101e829d2c3
mustaphouni04 <elaichounimustapha@gmail.com>
2024-09-19 12:49:55 +0200

    testing template matching robustness

--- src/edges.py
+++ src/edges.py
@@ -3,9 +3,9 @@ import cv2
 import numpy as np
 
 # downscale img
-img = cv2.imread('../images/Test/Frontal/Frontal/1062FNT.jpg', cv2.IMREAD_GRAYSCALE)
-width = 1920
-height = 1080
+img = cv2.imread('../../images/Test/Frontal/Frontal/1062FNT.jpg', cv2.IMREAD_GRAYSCALE)
+width = 1600
+height = 900
 coord = (width, height)
 
 re_img = cv2.resize(img, coord, interpolation=cv2.INTER_AREA)
@@ -28,7 +28,7 @@ for contour in contours:
     x, y, w, h = cv2.boundingRect(contour) # create a bounding rectangle for the contour (x,y) coords are top left side of rectagle
     aspect_ratio = float(w)/h
     area = cv2.contourArea(contour)
-    if aspect_ratio > 2 and area > 1000: # heuristic to find license plate
+    if aspect_ratio > 1 and area > 1000: # heuristic to find license plate
         # draw a bounding rectangle around the contour
         cv2.rectangle(re_img, (x, y), (x+w, y+h), (0, 255, 0), 2)
         # extract the region of interest
